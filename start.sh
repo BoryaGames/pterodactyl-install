db53b3b113fc4c650470c6e04a65baee98595ba6
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2024-12-20 17:53:39 +0500

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~
-curl "https://raw.githubusercontent.com/BoryaGames/pterodactyl-install/refs/heads/main/pterodactyl-install.sh" -o pterodactyl-install.sh
-chmod +x pterodactyl-install.sh
-./pterodactyl-install.sh "$@"
